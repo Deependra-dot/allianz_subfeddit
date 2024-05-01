1b2c3d6e9b13625b4a4053aada03d0b3efecb521
nep1bot <nepbot@gmail.com>
2024-05-01 23:44:55 +0530

    removing linting error from util package

--- util/timezone.py
+++ util/timezone.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
+
+"""
+this is a util file to find out the utc time corresponding to the unix timestamp
+"""
 # Unix timestamp to convert
-timestamp = 1714526641
+time_stamp = 1714526641
 
 # Convert Unix timestamp to UTC datetime object
-utc_time = datetime.fromtimestamp(timestamp)
+utc_time = datetime.fromtimestamp(time_stamp)
 
 # Print the UTC time in a specific format
 utc_time_str = utc_time.strftime('%Y-%m-%d %H:%M:%S')
